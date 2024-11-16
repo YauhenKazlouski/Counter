1036c18ef90143e13d972f5e2af27bd5be730ac3
YauhenKazlouski <yauhenkazlouskiswift@gmail.com>
2024-11-16 18:12:38 +0300

    Implemented counter change logic

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,12 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var counter = 0
+    
+    @IBOutlet weak var historyOfChengesView: UIView!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func buttonDecreaseCounter(_ sender: Any) {
+        counter -= 1
+        changeCounterLabel()
+    }
+    
+    @IBAction func buttonResetCounter(_ sender: Any) {
+        counter = 0
+        changeCounterLabel()
+    }
+    
+    @IBAction func buttonIncreaseCounter(_ sender: Any) {
+        counter += 1
+        changeCounterLabel()
+    }
+    
+    private func changeCounterLabel () {
+        counterLabel.text = "Значение счетчика: \(counter)"
+    }
 }
 
